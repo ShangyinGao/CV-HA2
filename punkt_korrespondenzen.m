dbd828a6e6f34998ea32843e53b416985b852c96
ShangyinGao <shangyingao@gmail.com>
2017-06-08 21:21:20 +0200

    init homework 2 of Computer Vision lecture

---
+++ punkt_korrespondenzen.m
@@ -0,0 +1,8 @@
+function [Korrespondenzen] = punkt_korrespondenzen(I1,I2,Mpt1,Mpt2,varargin)
+% In dieser Funktion sollen die extrahierten Merkmalspunkte aus einer
+% Stereo-Aufnahme mittels NCC verglichen werden um Korrespondenzpunktpaare
+% zu ermitteln.
+
+end
+
+
