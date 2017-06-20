375e115f9a9275ac2d7fa1546892a221f1764ad5
ShangyinGao <shangyingao@gmail.com>
2017-06-20 12:27:35 +0200

    update plot section

--- punkt_korrespondenzen.m
+++ punkt_korrespondenzen.m
@@ -52,33 +52,6 @@ end
 Korrespondenzen = Korrespondenzen(:, 2:end);
 
 %%
-% % Loesung II
-% NCC_tmp = zeros(numberP2, 1);
-% NCCMaxForEachP1 = zeros(numberP1, 2);
-% einMatrix = ones(window_length, 1);
-% for i = 1:numberP1
-%     windowsP1_tmp = I1(Mpt1(2, i)-(window_length-1)/2 : Mpt1(2, i)+(window_length-1)/2, ...
-%         Mpt1(1, i)-(window_length-1)/2 : Mpt1(1, i)+(window_length-1)/2);
-%     windowsP1BiasNorm_tmp = double(windowsP1_tmp) - mean(mean(windowsP1_tmp));
-% %     windowsP1BiasGainNorm_tmp = windowsP1BiasNorm_tmp./(sqrt((norm(windowsP1BiasNorm_tmp))^2/(N-1)));
-%     windowsP1BiasGainNorm_tmp = windowsP1BiasNorm_tmp./std(reshape(windowsP1BiasNorm_tmp, [], 1));
-%     
-% 
-%     for j = 1:numberP2
-%         windowsP2_tmp = I2(Mpt2(2, j)-(window_length-1)/2 : Mpt2(2, j)+(window_length-1)/2, ...
-%             Mpt2(1, j)-(window_length-1)/2 : Mpt2(1, j)+(window_length-1)/2);
-%         windowsP2BiasNorm_tmp = double(windowsP2_tmp) - mean(mean(windowsP2_tmp));
-% %         windowsP2BiasGainNorm_tmp = windowsP2BiasNorm_tmp./(sqrt((norm(windowsP2BiasNorm_tmp))^2/(N-1)));
-%         windowsP2BiasGainNorm_tmp = windowsP2BiasNorm_tmp./std(reshape(windowsP2BiasNorm_tmp, [], 1));
-%         NCC_tmp(j) = trace(windowsP1BiasGainNorm_tmp'*windowsP2BiasGainNorm_tmp)/(N-1);
-% 
-%     end
-%     [M, I] = max(NCC_tmp);
-%     NCCMaxForEachP1(i, :) = [M, I];
-%     
-% end
-
-%%
 % ploten
 imshow([I1, ones(size(I1, 1), 10), I2]); hold on;
 plot(Korrespondenzen(2, :)', Korrespondenzen(1, :)','rx'); hold on;
